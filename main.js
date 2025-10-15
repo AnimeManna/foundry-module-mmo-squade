dae8f1fb0a4b2c7f8e02689cc0082227df59ffdc
Maksym Hladko <maxuniwersal@gmail.com>
2025-10-15 18:58:19 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,47 @@
+Hooks.once('init', () => {
+  console.log('Mmo-squade module has been initalized');
+});
+
+const GUIDING_EFFECT_NAME = "Метка Уз";
+
+/**
+ * @param {Actor5e} actor 
+ * @param {TokenDocument5e} target 
+ * @param {object} rollConfig 
+ */
+async function addBonus(config, actor) {
+  
+  const target = game.user.targets.first();
+  if (!target) return;
+
+  const guidingBondEffect = target.actor.effects.find(effect => effect.name === GUIDING_EFFECT_NAME);
+
+  if (!guidingBondEffect) return;
+
+  if (guidingBondEffect.origin.includes(actor.uuid)) {
+    
+  config.rolls[0].parts ??= [];
+  config.rolls[0].parts.push('1d4');
+     
+  }
+}
+
+Hooks.on('dnd5e.preRollFormula', (...args) => {
+  console.log('attack',args)
+});
+
+Hooks.on('dnd5e.preRollAttack', (config) => {
+  addBonus(config, config.subject.actor)
+});
+
+Hooks.on('dnd5e.preRollAbilityCheck', (config) => {
+  console.log(config);
+  addBonus(config, config.subject);
+});
+
+Hooks.on('dnd5e.preRollSavingThrow', (config) => {
+  console.log(config);
+  addBonus(config, config.subject);
+});
+
+
